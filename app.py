5bbb212f451f15590bbc469981683bd0ab9888e7
Bilal-SSDL <bilal.abbasi620@gmail.com>
2024-04-27 13:18:48 +0500

    Added and rendered  html templates

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app_inst = Flask(__name__)
 
 @app_inst.route("/")
-def hello():
-    return "First flask app"
+def home():
+    #return "To-Do List Home Page"
+    return render_template('home.html')
+
+@app_inst.route("/add")
+def add_task():
+    return render_template("add_task.html")
+
+@app_inst.route("/update")
+def update_task():
+    return render_template('update_task.html')
+
+@app_inst.route("/delete")
+def delete_task():
+    return render_template('delete_task.html')
 
 if __name__ == "__main__":
-    app_inst.run()
+    app_inst.run(debug=True)
