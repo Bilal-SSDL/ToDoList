0acb0a4999bbb058b04795618fd9a1430204198b
Bilal-SSDL <bilal.abbasi620@gmail.com>
2024-04-28 22:35:11 +0500

    Added update, delete, get and task list

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def get_db_cursor(connection):
 def create_db_table():
     connection = get_db_connection()
     cursor = get_db_cursor(connection)
-    cursor.execute('''CREATE TABLE IF NOT EXISTS tasks (
-                   id INTEGER PRIMARY_KEY,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tasks_new (
+                   id INTEGER PRIMARY KEY,
                    task TEXT
     )''')
     connection.commit()
@@ -34,8 +34,14 @@ def create_db_table():
 
 @app_inst.route("/" , methods=['GET', 'POST'])
 def home():
-    #return "To-Do List Home Page"
-    return render_template('home.html')
+
+        connection = get_db_connection()
+        cursor = get_db_cursor(connection)
+        data = cursor.execute("SELECT * FROM tasks_new").fetchall()
+        #connection.commit()
+        connection.close()
+        print(data)
+        return render_template('home.html', tasks=data)
 
 @app_inst.route("/add", methods=['GET', 'POST'])
 def add_task():
@@ -44,20 +50,42 @@ def add_task():
         create_db_table()
         connection = get_db_connection()
         cursor = get_db_cursor(connection)
-        cursor.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
+        cursor.execute("INSERT INTO tasks_new (task) VALUES (?)", (task,))
         connection.commit()
         connection.close()
         return jsonify({"message": "Task added successfully"}), 200
     elif request.method == "GET":
         return render_template("add_task.html")
 
-@app_inst.route("/update", methods=['GET', 'POST'])
+@app_inst.route("/update", methods=['GET', 'PUT'])
 def update_task():
-    return render_template('update_task.html')
+    if request.method == "PUT":
+        task = request.json.get("task")
+        id   = request.json.get("id")
+        print("Task ", task)
+        print("ID ", id)
+        connection = get_db_connection()
+        cursor = get_db_cursor(connection)
+        cursor.execute("UPDATE tasks_new SET task= ? where id = ?", (task, id,))
+        connection.commit()
+        connection.close()
+        return jsonify({"message": "Task Updated successfully"}), 200
+    elif request.method == "GET":
+        return render_template('update_task.html')
 
-@app_inst.route("/delete", methods=['GET', 'POST'])
+@app_inst.route("/delete", methods=['GET', 'DELETE'])
 def delete_task():
-    return render_template('delete_task.html')
+    if request.method == "DELETE":
+        #task = request.json.get("task")
+        id   = request.json.get("id")
+        connection = get_db_connection()
+        cursor = get_db_cursor(connection)
+        cursor.execute("DELETE FROM tasks_new WHERE id= ?", ( id,))
+        connection.commit()
+        connection.close()
+        return jsonify({"message": "Task Deleted successfully"}), 200
+    elif request.method == "GET":
+        return render_template('delete_task.html')
 
 if __name__ == "__main__":
     app_inst.run(debug=True)
